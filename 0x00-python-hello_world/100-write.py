7ab5fc6ed17a022bd0f9e83773a71d6991c741c9
Houssame Ait hsain <hait-hsa@c1r8p1.1337.ma>
2024-03-11 17:16:35 +0000

    100\n

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import os
-os.write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19".encode('utf-8'))
+os.write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n".encode('utf-8'))
 exit(1)
