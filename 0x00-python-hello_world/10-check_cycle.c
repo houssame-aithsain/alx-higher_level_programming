522e1dc046aa444ea6f1b4270ed48d07428accfe
Houssame Ait hsain <hait-hsa@c1r8p4.1337.ma>
2024-03-11 21:57:19 +0000

    10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -15,22 +15,11 @@
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow, *fast;
-
-    if (list == NULL || list->next == NULL)
-        return (0);
-
-    slow = list;
-    fast = list->next;
-
-    while (fast != NULL && fast->next != NULL)
-    {
-        if (slow == fast)
-            return (1);
-
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-
-    return (0);
+	while (list)
+	{
+		if (list < list->next || list == list->next)
+			return (1);
+		list = list->next;
+	}
+	return (0);
 }
