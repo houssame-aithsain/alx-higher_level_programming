2c940a38d6e7bfa996d9425b1d4a8312d4635c70
Houssame Ait hsain <hait-hsa@c1r8p4.1337.ma>
2024-03-11 21:52:05 +0000

    102

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-import dis
-
 def magic_calculation(a, b):
     return 98 + (a ** b)
-
-code_obj = magic_calculation.__code__
-bytecode = dis.Bytecode(code_obj)
-line_numbers = dict(dis.findlinestarts(code_obj))
-
-for instr in bytecode:
-    line_number = line_numbers.get(instr.offset, '')
-    print(f"{line_number:2} {instr.offset:2} {instr.opname:20}{instr.argrepr}")
