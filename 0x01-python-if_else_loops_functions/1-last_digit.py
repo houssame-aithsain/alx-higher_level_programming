2627b0fa1ff7ecf53e3368a71e7dd853ae2735da
Houssame Ait hsain <hait-hsa@c1r8p1.1337.ma>
2024-03-12 22:39:06 +0000

    f

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number} is {str(number)[-1]}", end=" ")
-if int(str(number)[-1]) == 0:
+last_digit = int(str(number)[-1])
+if (number < 0):
+    last_digit *= -1
+print(f"Last digit of {number} is {last_digit}", end=" ")
+if last_digit == 0:
     print('and is 0')
-elif int(str(number)[-1]) > 5:
+elif last_digit > 5:
     print('and is greater than 5')
-elif int(str(number)[-1]) < 6:
+elif last_digit < 6:
     print('and is less than 6 and not 0')
