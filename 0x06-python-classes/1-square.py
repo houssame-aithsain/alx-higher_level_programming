e66094ff2f49589a584bf5c086cbd3185f606268
Houssame Ait hsain <hait-hsa@c1r7p4.1337.ma>
2024-04-03 02:31:57 +0000

    1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,26 +1,10 @@
 #!/usr/bin/python3
-"""
-Module: 1-square
-Defines a class Square with a private instance attribute size.
-"""
+"""Module: 1-square"""
 
 
 class Square:
-    """
-    Represents a square.
-
-    Attributes:
-        __size (int): The size of the square.
-    """
+    """Represents a square."""
 
     def __init__(self, size):
-        """
-        Initializes a new Square instance.
-
-        Parameters:
-            size (int): The size of the square.
-
-        Returns:
-            None.
-        """
+        """Initializes a new Square instance."""
         self.__size = size
