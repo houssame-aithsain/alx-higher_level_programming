a3fcf5c537d50a1063eab15a374b05f300cd1a96
Houssame Ait hsain <hait-hsa@c1r7p4.1337.ma>
2024-04-03 02:28:22 +0000

    5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+"""
+Module: 5-square
+Defines a class Square with private instance attribute size, size validation,
+a getter method to retrieve size, a setter method to update size with validation,
+public instance methods to calculate the area and print the square.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Parameters:
+            size (int): The size of the square (default 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+
+        Returns:
+            None.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Getter method to retrieve the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to update the size of the square.
+
+        Parameters:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+
+        Returns:
+            None.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Prints the square with the character '#' to stdout.
+
+        Returns:
+            None.
+        """
+        if self.__size == 0:
+            print()
+            return
+
+        for _ in range(self.__size):
+            print("#" * self.__size)
