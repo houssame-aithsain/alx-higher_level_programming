54ae015e39a6a103d0a131b945e025c97468a6bd
Houssame Ait hsain <hait-hsa@c1r7p4.1337.ma>
2024-04-03 02:23:05 +0000

    1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
 class Square:
+    """Represents a square with a private instance attribute 'size'."""
+
     def __init__(self, size):
+        """Initializes the square with the given size."""
         self.__size = size
