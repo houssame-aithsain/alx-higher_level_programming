24cb99eb1700d37b40de30e49515ef5d9ad15fb4
Houssame Ait Hsain <houssame1337@gmail.com>
2024-04-16 17:56:56 +0100

    100%

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Define a Student class."""
+"""Create a class Student that defines a student."""
 
 
 class Student:
-    ''' Student class '''
+    """Create a class Student that defines a student."""
 
     def __init__(self, first_name, last_name, age):
-        ''' Constructor method '''
+        """Initialize a new Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        ''' Method that returns directory description '''
+        """Return the dictionary description of a Student."""
         return self.__dict__.copy()
