3b935ed7ea7c45cb8c82f94f0f7cbe9e8c966300
Houssame Ait Hsain <houssame1337@gmail.com>
2024-04-16 13:51:10 +0100

    100%

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"test"
+"""Module for reading a text file."""
 
 
 def read_file(filename=""):
-    "test2"
+    """Read and print the contents of a text file to stdout."""
     with open(filename, encoding='utf-8') as file:
         data = file.read()
         print(data, end='')
