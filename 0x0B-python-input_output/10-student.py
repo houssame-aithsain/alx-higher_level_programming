218618c0113968a5b879f24d026a7892074d4196
Houssame Ait Hsain <houssame1337@gmail.com>
2024-04-16 17:59:35 +0100

    100%

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,14 +3,17 @@
 
 
 class Student:
-    """Define a Student class."""
+    """Defines a student by first_name, last_name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student."""
+        """Initialize a Student instance with first_name, last_name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Return the dictionary description of a Student."""
-        return self.__dict__.copy()
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
