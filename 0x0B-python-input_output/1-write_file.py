ae63772ce8ca90157576329751e4b88e094718da
Houssame Ait Hsain <houssame1337@gmail.com>
2024-04-16 14:04:44 +0100

    100%

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,4 +4,4 @@
 def write_file(filename="", text=""):
     """Write content to a file or create it if it doesn't exist."""
     with open(filename, "w", encoding='utf-8') as file:
-        file.write(text)
+        return file.write(text)
