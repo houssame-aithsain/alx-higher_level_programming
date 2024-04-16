b5bd5616623fae14d6e6aac35c3e5184a6e1be27
Houssame Ait Hsain <houssame1337@gmail.com>
2024-04-16 18:03:24 +0100

    100%

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,21 +3,22 @@
 
 
 class Student:
-    """Defines a student by first_name, last_name, and age."""
+    ''' Student class '''
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance with first_name, last_name, and age."""
+        ''' Constructor method '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return the dictionary description of a Student."""
+        ''' Method that returns directory description with filter '''
+
         if isinstance(attrs, list) and all(isinstance(attr, str)
-            for attr in attrs):
-                res = {}
-                for i in attrs:
-                    if i in self.__dict__:
-                        res[i] = self.__dict__[i]
-                return res
+                                           for attr in attrs):
+            res = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    res[i] = self.__dict__[i]
+            return res
         return self.__dict__
