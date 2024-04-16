81fd08be6058b1842536c501e8cf1238f3f69c7e
Houssame Ait Hsain <houssame1337@gmail.com>
2024-04-16 17:39:43 +0100

    100%

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,25 +3,15 @@
 
 
 import sys
-from os import path
+import os.path
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+items = []
+if os.path.exists('add_item.json'):
+    items = load_from_json_file('add_item.json')
 
-def add_to_list_and_save(arguments):
-    """Add command-line arguments to a list and save it to a JSON file."""
-    filename = "add_item.json"
-    if path.exists(filename):
-        my_list = load_from_json_file(filename)
-    else:
-        my_list = []
-
-    my_list.extend(arguments)
-
-    save_to_json_file(my_list, filename)
-
-if __name__ == "__main__":
-    arguments = sys.argv[1:]
-    add_to_list_and_save(arguments)
+items.extend(sys.argv[1:])
+save_to_json_file(items, 'add_item.json')
