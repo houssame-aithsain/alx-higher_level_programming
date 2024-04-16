0fac83dde4da05285cab66cae26e2d9a4e03ebc6
Houssame Ait Hsain <houssame1337@gmail.com>
2024-04-16 17:50:10 +0100

    100%

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,14 +5,17 @@
 class Student:
     """Defines a student by first_name, last_name, and age."""
 
+
     def __init__(self, first_name, last_name, age):
         """Initialize a Student instance with first_name, last_name, and age."""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
         """Retrieve a dictionary representation of a Student instance."""
+        
         return {
             'first_name': self.first_name,
             'last_name': self.last_name,
