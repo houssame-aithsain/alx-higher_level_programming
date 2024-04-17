7eeb683fd68939ed6a92bc4cd7fdf342f26f6312
Houssame Ait Hsain <houssame1337@gmail.com>
2024-04-17 22:30:34 +0100

    100%

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for Square class."""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A subclass representing a square."""
+    def __init__(self, size):
+        """Initializes a new Square object."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
