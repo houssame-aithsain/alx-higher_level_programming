0891b41f3428df6114212e62b24d49f480fa6286
Houssame <hossame@inbox.ru>
2024-05-21 23:20:05 +0100

    101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Create a table temperatures with the following structure:
-SELECT `city`, AVG(`value`) AS `avg_temp`
-FROM `temperatures`
-GROUP BY `city`
-ORDER BY `avg_temp` DESC;
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
