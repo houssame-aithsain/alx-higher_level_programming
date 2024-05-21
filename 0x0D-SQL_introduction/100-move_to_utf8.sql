7214496a9c7ae32a2227972a30fe3894611877fd
Houssame <hossame@inbox.ru>
2024-05-21 23:12:15 +0100

    100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- Version: 1.0
-USE hbtn_0c_0;
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `hbtn_0c_0`
+ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8 COLLATE utf8_unicode_ci;
