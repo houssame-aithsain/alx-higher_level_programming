0cb1fd41e49ac74108a032c3a349982716766c52
Houssame <hossame@inbox.ru>
2024-05-21 23:13:03 +0100

    100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- Version: 1.0
 USE `hbtn_0c_0`
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE 'first_table' CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
