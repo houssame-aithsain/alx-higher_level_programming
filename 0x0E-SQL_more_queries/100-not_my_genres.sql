50826cb98c6fdf5c9e94709a3e2d69af1c1bc314
Houssame <hossame@inbox.ru>
2024-05-22 21:31:34 +0100

    100

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- Count the number of genres that are not associated with the show Dexter.
+SELECT name
+FROM tv_genres
+WHERE id NOT IN (
+    SELECT genre_id
+    FROM tv_show_genres
+    WHERE show_id = (
+        SELECT id
+        FROM tv_shows
+        WHERE title = 'Dexter'
+    )
+)
+ORDER BY name ASC;
