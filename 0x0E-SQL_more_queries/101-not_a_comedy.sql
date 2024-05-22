a59932e91c7915a77db0ef1a1cb4a4b975d63edd
Houssame <hossame@inbox.ru>
2024-05-22 21:32:25 +0100

    101

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- Find the titles of all TV shows that are not in the Comedy genre.
+SELECT title
+FROM tv_shows
+WHERE id NOT IN (
+    SELECT show_id
+    FROM tv_show_genres
+    WHERE genre_id = (
+        SELECT id
+        FROM tv_genres
+        WHERE name = 'Comedy'
+    )
+)
+ORDER BY title ASC;
