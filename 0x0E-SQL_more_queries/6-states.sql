3a3895dc98467b2877a00e7b3b816a675a81ec8f
Houssame <hossame@inbox.ru>
2024-05-22 21:09:58 +0100

    6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- this script creates a database named hbtn_0d_usa and a table named states with the following fields:
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
+
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");
