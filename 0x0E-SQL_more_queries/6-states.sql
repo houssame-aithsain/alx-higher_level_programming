ba9fc0564fa17cefafb57f83e24c1ceff18d60e1
Houssame <hossame@inbox.ru>
2024-05-22 21:11:42 +0100

    6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,11 +2,8 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-USE hbtn_0d_usa;
-
 CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
-
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");
