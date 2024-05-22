c3f9b7213d758140377daf779e3a837f3acad1c3
Houssame <hossame@inbox.ru>
2024-05-22 21:36:30 +0100

    102

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- 102-rating_shows.sql
--- Show all shows and their ratings, ordered by rating
-SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating_sum
-FROM tv_shows
-JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+-- Show the title and rating of all TV shows, ordered by rating in descending order.
+SELECT t.title, SUM(r.rate) AS rating
+FROM tv_shows t
+INNER JOIN tv_show_ratings r ON t.id = r.show_id
+GROUP BY t.title
+ORDER BY rating DESC;
