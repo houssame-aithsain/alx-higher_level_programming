1ffef59da980fa744b48ff979a87f74b9db310d5
Houssame <hossame@inbox.ru>
2024-05-22 21:25:50 +0100

    14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
--- Show the genre of the TV show Dexter.
+-- Order all the genres of the TV show Dexter alphabetically.
 SELECT name FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter';
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY name;
