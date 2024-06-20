4a9cdf1548c678911336ac7de9b014686bc51c30
Houssame <houssame1337@gmail.com>
2024-06-20 19:50:34 -0400

    0

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // script that increments and calls a function.
 
-exports.callMeMoby = function (number, theFunction) {
+exports.addMeMaybe = function (number, theFunction) {
   theFunction(++number);
 };
