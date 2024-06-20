2b2cf3e8a57082ffd7aaa65a120fb9ee64225ffb
Houssame <houssame1337@gmail.com>
2024-06-20 19:34:19 -0400

    0

--- 0x12-javascript-warm_up/10-arguments.js
+++ 0x12-javascript-warm_up/10-arguments.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 // Script that passed:
 
-const n = parseInt(process.argv[2]);
+const n = process.argv[2];
 
-function factorial (n) {
-  if (isNaN(n) || n <= 1) {
+function fac (n) {
+  if (n <= 1 || isNaN(n)) {
     return 1;
   } else {
-    return n * factorial(n - 1);
+    return n * fac(n - 1);
   }
 }
 
-console.log(factorial(n));
+console.log(fac(n));
