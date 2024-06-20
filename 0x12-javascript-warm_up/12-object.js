24025d769ebb00571ffe50073feb8591f9d7dc56
Houssame <houssame1337@gmail.com>
2024-06-20 19:38:16 -0400

    0

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Execute by adding the following line to ~/.bashrc
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+myObject.value = 89;
+
+console.log(myObject);
+console.log(myObject);
