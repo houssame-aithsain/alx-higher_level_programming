5695b14f658ce2bd3dd7ce731e5f470279570f3d
Houssame <houssame1337@gmail.com>
2024-06-20 20:23:22 -0400

    0

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Write a class Square that defines a square and inherits from Rectangle of 4-rectangle.js:
+
+class Square extends require('./5-square') {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
