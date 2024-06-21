d9c9173e47e46ab1261fd3fe8554e993eb2c88c7
Houssame <houssame1337@gmail.com>
2024-06-20 20:34:51 -0400

    0

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,18 +2,6 @@
 // Write a script that concats 2 files.
 
 const fs = require('fs');
-
-const concatFiles = (sourceFile1, sourceFile2, destinationFile) => {
-  try {
-    const content1 = fs.readFileSync(sourceFile1, 'utf8');
-    const content2 = fs.readFileSync(sourceFile2, 'utf8');
-    const concatenatedContent = content1 + '\n' + content2;
-    fs.writeFileSync(destinationFile, concatenatedContent);
-    console.log('Files concatenated successfully!');
-  } catch (error) {
-    console.error('An error occurred:', error);
-  }
-};
-
-const [sourceFile1, sourceFile2, destinationFile] = process.argv.slice(2);
-concatFiles(sourceFile1, sourceFile2, destinationFile);
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);
