e89e52e940310fce19fa94fdec2605db45349f38
Houssame <houssame1337@gmail.com>
2024-06-23 20:17:23 -0400

    00

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,36 +1,24 @@
 #!/usr/bin/python3
 """
-this script lists all states from the database
-hbtn_0e_0_usa where the name starts with N
+Lists all states with a name starting with N from the database hbtn_0e_0_usa
 """
 
 
+from sys import argv
 import MySQLdb
-import sys
 
 
-if __name__ == "__main__":
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+if __name__ == '__main__':
+    database = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], database=argv[3])
 
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    cur = database.cursor()
 
-    # Create a cursor object
-    cursor = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name\
+                LIKE BINARY 'N%' ORDER BY id ASC")
 
-    # Execute the query
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    # Fetch all the rows
-    rows = cursor.fetchall()
-
-    # Print the results
-    for row in rows:
-        print(row)
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    cur.close()
+    database.close()
