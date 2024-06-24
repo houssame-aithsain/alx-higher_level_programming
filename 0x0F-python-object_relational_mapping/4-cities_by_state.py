1fcffea0ee15cc34026334438df05cd609b02b9d
Houssame <houssame1337@gmail.com>
2024-06-23 20:37:37 -0400

    00

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+takes in the name of a state as an argument and lists all cities of that state
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                    JOIN states ON cities.state_id = states.id\
+                    ORDER BY cities.id ASC")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
