213b8578297953a29636cd82bdddb9f0625bb906
Houssame <houssame1337@gmail.com>
2024-07-04 21:00:15 -0400

    00

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL
-curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
+# This script sends a request to a URL and displays the size of the body of the response in bytes
+curl -s "$1" | wc -c
