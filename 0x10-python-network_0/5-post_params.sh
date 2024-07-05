6ed664f541c01e98906ab5b7626e8edc98a1109f
Houssame <houssame1337@gmail.com>
2024-07-04 21:09:43 -0400

    00

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request with email and subject parameters and displays the response body
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I%30will%30always%30be%30here%30for%30PLD" "$1"
