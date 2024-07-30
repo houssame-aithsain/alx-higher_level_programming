1747ab9a7d7c70368c72e6a3bb746a73c674d956
Elpira <elpira@elpira.168.1.36>
2024-07-30 16:47:30 -0400

    js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -14,7 +14,7 @@ request(apiUrl, (error, response, body) => {
   const films = JSON.parse(body).results;
 
   const wedgeAntillesCount = films.reduce((count, film) => {
-    const hasWedgeAntilles = film.characters.some(characterUrl => 
+    const hasWedgeAntilles = film.characters.some(characterUrl =>
       characterUrl.endsWith(`/api/people/${wedgeAntillesId}/`)
     );
     return hasWedgeAntilles ? count + 1 : count;
