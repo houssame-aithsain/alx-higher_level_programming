769415ab6d84183df7ebb225e8b6c42c691d4047
Elpira <elpira@elpira.168.1.36>
2024-07-30 16:25:52 -0400

    js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,12 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
+    console.error(err);
+    return;
   }
+  console.log(data);
 });
